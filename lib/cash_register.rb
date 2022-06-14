d8090a67be777c60daa0076968e7e84e659c1e0e
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-06-14 10:59:59 +0300

    partially complete

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+    attr_accessor:total,:discount
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+    end 
+    
+    def add_item(title,price,*quantity)
+        title
+        quantity.sum do |num|
+            self.total += num * price
+        end 
+      
+    end 
+        
+end 
 
